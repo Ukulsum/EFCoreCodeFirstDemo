9c7c7cbcd09c8f5ab5b2a0b9b7f6b18d348f8e87
ukulsum <ukulsum1280@gmail.com>
2025-04-05 07:29:02 +0600

    dbcontext

---
+++ EFCoreCodeFirstDemo/Entities/EFCoreDbContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreCodeFirstDemo.Entities
+{
+    // EFCoreDbContext class inherits from DbContext, which is the primary class for interacting with the database using EF Core.
+    public class EFCoreDbContext : DbContext
+    {
+        // Constructor that accept DbContextOptions<EFCoreDbContext> as a parameter.
+        // The options parameter contains the settings required by EF Core to configure the DbContext,
+        // Such as the connection string and provider.
+        public EFCoreDbContext(DbContextOptions<EFCoreDbContext> options) : base(options) 
+        {
+            
+        }
+
+
+        // OnConfiguring is an overirde method that allows configuring the DbContext options,
+        // like setting the database provider and connection string.
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Configure the database provider and connection string.
+            // UseSqlServer method configures the DbContext to use SQL Server as the database provider.
+
+        }
+
+
+
+
+
+        // DbSet<Student> Students represents a table in the database corresponding to the Student entity.
+        // EF Core uses DbSet<TEntity> to track changes and execute queries relatedto the Student entity.
+        public DbSet<Student> Students { get; set; }
+
+        // DbSet<Branch> Branches represents a table in the database corresponding to the Branch entity.
+        // Similar to the Students DbSet, this property is used by EF Core to track and manage Branch entities.
+        public DbSet<Branch> Branches { get; set; }
+    }
+}
---
+++ EFCoreCodeFirstDemo/Entities/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreCodeFirstDemo.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string? Gender { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+
+        // Navigation property representing the Branch the student is enrolled in
+        public virtual Branch? Branch { get; set; }
+    }
+}
